5407742af2a79480255dab250e171b046bf3716b
Cuel <Cuel@users.noreply.github.com>
2018-04-14 10:40:54 +0200

    handle null database

--- lib/database.ts
+++ lib/database.ts
@@ -18,12 +18,13 @@ export interface Event {
 
 export class Database {
   private log = LoggerFactory.create(Database);
-  db: mongodb.Db;
+  db: mongodb.Db | null = null;
+
   constructor(private readonly mongo: mongodb.MongoClient) {
   }
 
   connect(url: string = ''): Promise<boolean> {
-    if (!url) return Promise.reject(new Error('Invalid url'));
+    if (!url) return Promise.reject(new Error('Empty url provided'));
 
     return new Promise((resolve, reject) => {
       this.mongo.connect(url, { autoReconnect: true }, (err, db) => {
@@ -41,16 +42,16 @@ export class Database {
 
   findOneUser(userNameOrSteamId: string): Promise<User> {
       const userQuery = { $or: [{ name: userNameOrSteamId }, { steam_id: userNameOrSteamId }] };
-      return this.db.collection('users').findOne(userQuery);
+      return this.db!.collection('users').findOne(userQuery);
   }
 
   countUserAttendance(userSteamid: string): Promise<number> {
       const groupQuery = { 'units.user_id': { $in: [userSteamid] } };
-      return this.db.collection('groups').count(groupQuery);
+      return this.db!.collection('groups').count(groupQuery);
   }
 
   async findFutureEvents(): Promise<Event[]> {
-    const collection = this.db.collection('events');
+    const collection = this.db!.collection('events');
     const query = { date: { $gte: new Date() } };
     const options = { sort: { date: 1 } };
 
