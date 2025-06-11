440a49bf62794f09717467863e10f4b6887cd9fe
Perondas <thomas@perondas.dev>
2025-06-11 18:18:22 +0200

    Improved error handling for PBO linting cont'

--- src/helpers/pbo_linter.rs
+++ src/helpers/pbo_linter.rs
@@ -1,6 +1,5 @@
 use crate::commands::upload::errors::PboUploadError;
 use anyhow::anyhow;
-use log::warn;
 use std::path::PathBuf;
 use tracing::info;
 use uuid::Uuid;
@@ -33,25 +32,16 @@ pub async fn lint_pbo(pbo_path: &PathBuf) -> Result<(), PboUploadError> {
         return Ok(());
     }
 
-    match res.status.code() {
-        Some(13 | 17 | 87) => {
-            let errors = String::from_utf8_lossy(&res.stdout);
-            let errors = errors
-                .lines()
-                .filter(|str| str.contains(folder.to_str().unwrap()))
-                .map(|str| str.to_string())
-                .map(|str| str.replace(folder.to_str().unwrap(), "").trim().to_owned())
-                .collect::<Vec<_>>()
-                .join("\n");
+    let out = String::from_utf8_lossy(&res.stdout);
+    let err = String::from_utf8_lossy(&res.stderr);
+    let out = format!("{}\n{}", out, err);
+    let errors = out
+        .lines()
+        .filter(|str| str.contains(folder.to_str().unwrap()))
+        .map(|str| str.to_string())
+        .map(|str| str.replace(folder.to_str().unwrap(), "").trim().to_owned())
+        .collect::<Vec<_>>()
+        .join("\n");
 
-            Err(PboUploadError::LintErrors(errors))
-        }
-        _ => {
-            warn!(
-                "PBO linting failed with unexpected status code: {:?}",
-                res.status.code()
-            );
-            Err(PboUploadError::LintError)
-        }
-    }
+    Err(PboUploadError::LintErrors(errors))
 }
